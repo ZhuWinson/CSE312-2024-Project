d69aa76ccdc06c2afe58dc89e65b71cc106c90e4
jem7119 <jackmazz10@gmail.com>
2024-10-11 13:39:28 -0400

    serving static files implemented

--- server.py
+++ server.py
@@ -1,30 +1,10 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path = "/static")
 
-# respond with "index.html" contents
-@app.route("/")
-def homehtml():
+@app.route("/", methods=["GET", "POST"])
+def home_page():
     return render_template("index.html")
 
-# if "filename" exists respond with contents of "filename"
-# else respond with 404 not found
-#
-# possibly simplify this by only serving one image?
-#
-@app.route("/templates/image/<filename>")
-def route_image(filename):
-    return render_template(filename)
-
-# respond with "style.css" contents
-@app.route("/templates/style.css")
-def route_css():
-    return render_template("style.css")
-
-# respond with "index.js" contents
-@app.route("/templates/index.js")
-def route_js():
-    return render_template("index.js")
-    
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host = "0.0.0.0", port = 8080, debug = True)
