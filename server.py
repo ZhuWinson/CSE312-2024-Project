3ed863a42c0e54457ead627151540caa957645a9
andempsey8 <andempsey8@gmail.com>
2024-12-04 15:31:21 -0500

    Added a flask_limiter for ip rate limiting with a cooldown of 30 seconds when >50 requests are submitted in 10 seconds

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
-from flask import Flask, make_response, redirect, request
+from flask import Flask, make_response, redirect, request, jsonify
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 from flask_socketio import SocketIO, emit
 from util.accounts import register, login, logout, purge_accounts
 from util.posts import create_post, delete_post, like_post, list_posts, list_recent_posts
@@ -13,6 +15,31 @@ import time
 
 app = Flask(__name__, static_url_path="/static")
 socketio = SocketIO(app)
+blocked_ips = {}
+
+def exceededLimit(reqLimit):
+    ip = get_remote_address()
+    blocked_ips[ip] = time.time()+30
+    return jsonify({"Request rate limit exceeded": "Please wait 30 seconds and try again"})
+
+limiter = Limiter(
+    key_func=get_remote_address,
+    app=app,
+    on_breach=exceededLimit
+)
+globalLimit = limiter.shared_limit("50 per 10 seconds", scope="*")
+
+@app.before_request
+@globalLimit
+def checkIP():
+    print("Before request")
+    ip = get_remote_address()
+    currTime = time.time()
+    if ip in blocked_ips:
+        if currTime < blocked_ips[ip]:
+            return jsonify({"Request rate limit exceeded": "Please wait 30 seconds and try again"}), 429
+        else:
+            del blocked_ips[ip]
 
 @app.after_request
 def after_request(response):
@@ -20,28 +47,34 @@ def after_request(response):
     return response 
 
 @app.route("/invalidpassword")
+@globalLimit
 def invalid_password():
     return "<p> Invalid Password </p>",{"Refresh": "1; url=http://localhost:8080/"}
 
 @app.route("/passwordmismatch")
+@globalLimit
 def mismatch_password():
     return "<p> Passwords Do Not Match </p>",{"Refresh": "1; url=http://localhost:8080/"}
 
 @app.route("/usertaken")
+@globalLimit
 def user_taken():
     return "<p> Username Taken </p>",{"Refresh": "1; url=http://localhost:8080/"}
 
 @app.route("/account", methods=["GET"])
+@globalLimit
 def account_page():
     auth_token = request.cookies.get("auth_token")
     return render_home_page("Account Info", "account_info", auth_token)
 
 @app.route("/home/<category>", methods=["GET"])
+@globalLimit
 def category_page(category):
     auth_token = request.cookies.get("auth_token")
     return render_home_page("/" + category, "post_list", auth_token)
 
 @app.route("/create", methods=["POST"])
+@globalLimit
 def create():
     auth_token = request.cookies.get("auth_token")
     title = html.escape(request.form.get("title", ""))
@@ -56,11 +89,13 @@ def create():
     return redirect("/home/" + category)
 
 @app.route("/create", methods=["GET"])
+@globalLimit
 def create_page():
     auth_token = request.cookies.get("auth_token")
     return render_home_page("Create Post", "create_form", auth_token)
 
 @app.route("/posts/<id>", methods=["DELETE"])
+@globalLimit
 def delete(id):
     auth_token = request.cookies.get("auth_token")
     authorized = delete_post(id, auth_token)
@@ -72,31 +107,37 @@ def delete(id):
 @app.route("/", methods=["GET"])
 @app.route("/home", methods=["GET"])
 @app.route("/home/", methods=["GET"])
+@globalLimit
 def home_page():
     return redirect("/home/recent")
 
 @app.route("/like/<id>", methods=["POST"])
+@globalLimit
 def like(id):
     auth_token = request.cookies.get("auth_token")
     like_post(id, auth_token)
     return make_response("", 204)
 
 @app.route("/login", methods=["POST"])
+@globalLimit
 def login_submit():
     username = html.escape(request.form.get("username"))
     password = request.form.get("password")
     return login(username, password)
 
 @app.route("/login", methods=["GET"])
+@globalLimit
 def login_page():
     auth_token = request.cookies.get("auth_token")
     return render_home_page("Login", "login_form", auth_token)
 
 @app.route("/logout", methods=["GET", "POST"])
+@globalLimit
 def logout_submit():
     return logout()
 
 @app.route("/register", methods=["POST"])
+@globalLimit
 def register_submit():
     username = html.escape(request.form.get("username"))
     password = request.form.get("password")
@@ -104,11 +145,13 @@ def register_submit():
     return register(username, password, verification)
 
 @app.route("/register", methods=["GET"])
+@globalLimit
 def register_page():
     auth_token = request.cookies.get("auth_token")
     return render_home_page("Register", "registration_form", auth_token)
 
 @app.route("/posts/<category>", methods=["GET"])
+@globalLimit
 def post_list(category):
     category = html.escape(category)
     post_list = None
@@ -122,6 +165,7 @@ def post_list(category):
     return response
 
 @app.route("/purge", methods=["GET"])
+@globalLimit
 def purge():
     dev_mode = os.environ.get("DEV_MODE")
     if dev_mode != "enabled":
@@ -137,10 +181,12 @@ def update():
         time.sleep(1)
 
 @socketio.on("connect")
+@globalLimit
 def ws_handshake():
     print("Client Connected")
 
 @socketio.on('delete')
+@globalLimit
 def ws_delete_post(post_id):
     auth_token = request.cookies.get("auth_token")
     authorized = delete_post(post_id, auth_token)
@@ -148,12 +194,18 @@ def ws_delete_post(post_id):
         emit('remove', post_id, broadcast=True)
 
 @socketio.on('like')
+@globalLimit
 def ws_like_post(post_id):
     auth_token = request.cookies.get("auth_token")
     authorized = like_post(post_id, auth_token)
     if authorized:
         post = retrieve_post(post_id)
         emit('update', post, broadcast=True)
+
+@app.errorhandler(429)
+def exceededLimit(e):
+    return jsonify({"Request rate limit exceeded": "Please wait 30 seconds and try again"}), 429
+
 
 if __name__ == "__main__":
     thread = threading.Thread(target=update)
